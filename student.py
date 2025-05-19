2351512fd1b28f62887822da3e211b27db9294b4
jaksamarko-sys <jaksa.marko@diak.szbi-pg.hu>
2025-05-19 10:18:35 +0200

    yes

--- student.py
+++ student.py
@@ -3,6 +3,12 @@ class Student:
     age = 0
     sex = ""
     score = 0
+
+    def __init__(self, name, age, sex):
+        self.name = name
+        self.age = age
+        self.sex = sex
+        self.score = 10
     
     def introduce(self):
         print(f"Név: {self.name}, Kor: {self.age}, Pontszám: {self.score}")
@@ -10,13 +16,7 @@ class Student:
     def learn(self, points):
         self.score += points 
 
-tivadar = Student()
-print(tivadar)
-
-tivadar.name = "El Tivadar"
-tivadar.age = 16
-tivadar.sex = "male"
-tivadar.score = 20
+tivadar = Student("El Tivadar", 16, "male")
 
 # print(f"Név: {tivadar.name}, Kor: {tivadar.age}, Pontszám: {tivadar.score}")
 tivadar.introduce()
